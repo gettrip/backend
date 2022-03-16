0287920af97d78384cbf1fe78f5d73c5b8115d93
ProtKsen <protcenko.kseniya@gmail.com>
2022-03-16 19:25:35 +0500

    chore/linter main fix

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,24 +1,29 @@
-from email import message
 import logging
+import os
+from http import HTTPStatus
 
 from flask import Flask
+from pydantic import ValidationError
+from werkzeug.exceptions import HTTPException
+
+from backend.errors import AppError
 from backend.views.cities import cities
 from backend.views.user import user
-from http import HTTPStatus
-from werkzeug.exceptions import HTTPException
-from backend.errors import AppError
-from pydantic import ValidationError
 
 logger = logging.getLogger(__name__)
 
+
 def handle_http_exceptions(error: HTTPException):
-    return {"message": error.description}, error.code
+    return {'message': error.description}, error.code
+
 
 def handle_app_error(error: AppError):
     return {'message': error.reason}, error.status
 
+
 def handle_validation_error(error: ValidationError):
     return error.json(), HTTPStatus.BAD_REQUEST
+
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
@@ -26,15 +31,19 @@ def main():
 
     app = Flask(__name__)
 
-    app.register_blueprint(cities, url_prefix='/api/v1/cities') 
+    app.register_blueprint(cities, url_prefix='/api/v1/cities')
     app.register_blueprint(user, url_prefix='/api/v1/users')
 
     app.register_error_handler(HTTPException, handle_http_exceptions)
     app.register_error_handler(AppError, handle_app_error)
     app.register_error_handler(ValidationError, handle_validation_error)
 
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(
+        host=os.environ['APP_HOST'],
+        port=os.environ['APP_PORT'],
+        debug=False,
+    )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
