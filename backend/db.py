3637f1883f628b715eb974d9fda629557a08e73d
ProtKsen <protcenko.kseniya@gmail.com>
2022-03-16 19:36:42 +0500

    chore/linter db fix

--- backend/db.py
+++ backend/db.py
@@ -2,11 +2,11 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 url = os.getenv('DB_URL')
 
-engine = create_engine('{}'.format(url))
+engine = create_engine('{url}'.format(url=url))
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
