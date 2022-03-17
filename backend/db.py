087e24cbbb487b68267398f359621279a647caeb
ProtKsen <protcenko.kseniya@gmail.com>
2022-03-17 19:47:48 +0500

    chore/create config

--- backend/db.py
+++ backend/db.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-import settings
+import config
 
-url = settings.DB_URL
+app_config = config.load_from_env()
+
+url = app_config.db.url
 
 engine = create_engine(url)
 db_session = scoped_session(sessionmaker(bind=engine))
---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+from pydantic import BaseModel
+
+
+class Server(BaseModel):
+    port: str
+    host: str
+
+
+class DataBase(BaseModel):
+    url: str
+
+
+class AppConfig(BaseModel):
+    server: Server
+    db: DataBase
+
+
+def load_from_env() -> AppConfig:
+    app_port = os.environ['APP_PORT']
+    app_host = os.environ['APP_HOST']
+    db_url = os.environ['DB_URL']
+    return AppConfig(
+        server=Server(port=app_port, host=app_host),
+        db=DataBase(url=db_url),
+    )
