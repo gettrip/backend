74806cd1c94e678de1e20da8baf8b63b28c1b7f8
Valexandrin <alex-vadim@mail.ru>
2022-03-20 09:55:35 +0300

    model user__repr__  bugfix

--- backend/models.py
+++ backend/models.py
@@ -20,7 +20,7 @@ class User(Base):
     username = Column(String(), unique=True)
 
     def __repr__(self) -> str:
-        return f'User {self.uid}, {self.name}'
+        return f'User {self.uid}, {self.username}'
 
 
 def main():
