19394cbddf74855c6c1515a5f122db59e735d4ea
ProtKsen <protcenko.kseniya@gmail.com>
2022-06-13 15:39:54 +0500

    feature: add get_db function for db_url

--- backend/db.py
+++ backend/db.py
@@ -2,12 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from backend.config import config
 
-url = config.db.url
+def get_db(url: str) -> scoped_session:
+    engine = create_engine(url)
+    return scoped_session(sessionmaker(bind=engine))
 
-engine = create_engine(url)
-db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
-Base.query = db_session.query_property()
