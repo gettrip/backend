1ed44f0554d7128663759cd79ef3289f71f373f8
ProtKsen <protcenko.kseniya@gmail.com>
2022-06-19 18:26:55 +0500

    feature: add RouteFactory

--- tests/factories.py
+++ tests/factories.py
@@ -1,13 +1,26 @@
 import factory
 
-from backend.models import City
+from backend.models import City, Route
 
 
 class CityFactory(factory.alchemy.SQLAlchemyModelFactory):
 
     uid = factory.Sequence(lambda n: '%s' % n)
-    name = factory.Faker('name')
+    name = factory.Faker('city')
     image = factory.Faker('email')
 
     class Meta:
         model = City
+
+
+class RouteFactory(factory.alchemy.SQLAlchemyModelFactory):
+
+    uid = factory.Sequence(lambda n: '%s' % n)
+    name = factory.Faker('city')
+    city_id = 1
+    image = factory.Faker('email')
+    description = factory.Faker('text')
+    duration = 100
+
+    class Meta:
+        model = Route
